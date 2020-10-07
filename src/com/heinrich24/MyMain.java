592ce7842e044a104ba551804743aaa2284d6118
Heinrich Dedekind <heinrich.dedekind@gmail.com>
2020-10-07 07:59:56 +0200

    Initial project foundation

---
+++ src/com/heinrich24/MyMain.java
@@ -0,0 +1,44 @@
+package com.heinrich24;
+
+import java.util.Scanner;
+
+public class MyMain {
+
+	/*
+	 * Anagram function to check if two strings are similar from a character
+	 * frequency point of view.
+	 */
+	static boolean isAnagram(String a, String b) {
+		// To get a good working set lets make all the cases the same.
+		String aUpperCase = a.toUpperCase();
+		String bUpperCase = b.toUpperCase();
+
+		// Lets leverage the Stream API to sort the String for us
+		String aSorted = aUpperCase.chars().sorted()
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+
+		String bSorted = bUpperCase.chars().sorted()
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+
+		/*
+		 * Now we check to see if the two string are the same. If they are the same we
+		 * return true else return false if they are not the same
+		 */
+		if (aSorted.equalsIgnoreCase(bSorted)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+		boolean ret = isAnagram(a, b);
+		System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+
+	}
+
+}
